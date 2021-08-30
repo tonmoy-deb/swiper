dcc1ba71e49573214639b6e1a843f9d8458cb05c
Tonmoy Deb <td.tonmoydeb@gmail.com>
2021-08-30 21:04:06 +0600

    Update app.js

--- app.js
+++ app.js
@@ -3,57 +3,54 @@ const container = document.querySelector('.container');
 let swipeEnabled = false;
 let swipeStartAt = 0;
 let swipeEndAt = 0;
+let swipeDistance = 100;
 
 
 // FUNCTIONS
 
-const swipeToLeft = () => {
-    alert("swipe to left")
+const rightToLeft = () => {
+    alert("right to left")
 }
 
-const swipeToRight = () => {
-    alert("swipe to right")
+const leftToRight = () => {
+    alert("left to right")
 }
 
 const swipeStart = (e) => {
     // ENABLE SWIPE
     swipeEnabled = true;
     // SET SWIPE START POINT
-    swipeStartAt = e.type.includes('touch') ? e.touches[0].clientX : event.pageX;
+    swipeStartAt = e.type.includes('touch') ? e.touches[0].clientX : e.clientX;
 
     // console.log(e)
 }
 
-const swipeMove = (e) => {
-    if (swipeEnabled) {
+const swipeEnd = (e) => {
+    if(swipeEnabled) {
         // DISABLE SWIPE
         swipeEnabled = false
         // SET SWIPE END POINT
-        swipeEndAt = e.type.includes('touch') ? e.touches[0].clientX : event.pageX;
+        swipeEndAt = e.type.includes('touch') ? e.changedTouches[0].clientX : e.clientX;
 
-        // console.log(e) 
-    }  
-}
-
-const swipeEnd = () => {
-
-    // console.log(swipeStartAt) 
-    // console.log(swipeEndAt)
-
-    if (swipeStartAt > swipeEndAt) {
-        swipeToLeft()
-    }else if (swipeStartAt < swipeEndAt) {
-        swipeToRight()
+        // CHECK SWIPE DIRECTION
+        if ((swipeStartAt - swipeEndAt) >= swipeDistance) {
+            rightToLeft()
+        }else if ((swipeEndAt - swipeStartAt) >= swipeDistance) {
+            leftToRight()
+        }
     }
+
+    // RESET SWIPE ENDPOINTS
+    swipeStartAt = 0;
+    swipeEndAt = 0;
 }
 
 // TOUCH EVENTS [MOBILE]
 container.addEventListener('touchstart', swipeStart);
-container.addEventListener('touchmove', swipeMove);
 container.addEventListener('touchend', swipeEnd);
 
 // MOUSE EVENTS [DESKTOP]
 container.addEventListener('mousedown', swipeStart);
-container.addEventListener('mousemove', swipeMove);
 container.addEventListener('mouseup', swipeEnd);
+container.addEventListener('mouseleave', swipeEnd);
 
